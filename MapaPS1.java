8fab5261197c4bf53b9fe49b8879ae906d2af556
Marina Schramm Jorge Schneider <84358232+marinasjs@users.noreply.github.com>
2022-01-18 08:37:01 -0300

    Add files via upload

---
+++ MapaPS1.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mapaps1;
+
+
+
+/**
+ *
+ * @author marina
+ */
+public class MapaPS1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        BoasVindas1 bv = new BoasVindas1();
+        bv.setVisible(true);
+        
+        
+       
+        
+    }
+    
+}
